59696a0caa557b43b02c6249963547ceb2001a21
Mike Lyons <mdl0394@gmail.com>
2013-04-10 22:44:33 -0400

    Collapsing doesn't remove open class

--- script.js
+++ script.js
@@ -7,21 +7,36 @@ function expand( target ) {
 	}
 }
 
+function collapse( target ) {
+	if( target ) {
+		$("#" + target).parents(".box_cont").addClass("hidden", ANIMATION_SPEED, "linear");
+		$("#" + target).parents(".box_cont").next(".box_cont").addClass("hidden", ANIMATION_SPEED, "linear");
+		$("#" + target).addClass("hidden", ANIMATION_SPEED, "linear");
+	}
+}
+
 $(document).ready( function() {
 	$(".line_button").click(function() {
 		var target = $(this).attr("data-target")
-		$(this).parents(".box_cont").next(".box_cont").find(".box").each(function() {
-			$(this).addClass("hidden");
-			console.log(target);
-		});
-		expand(target);
+		if( !$(this).hasClass("open") ) {
+			$(this).addClass("open");
+
+			$(this).parents(".box_cont").next(".box_cont").find(".box").each(function() {
+				$(this).addClass("hidden");
+				console.log(target);
+			});
+
+			expand(target);
+		} else {
+			$(this).removeClass("open");
+			collapse(target);
+		}
 	});
 
 	$(".box_cont").hover(function() {
 		$(this).removeClass("box_cont_slim", ANIMATION_SPEED, "linear");
 		$(this).prev(".box_cont").each(function() {
 			$(this).addClass("box_cont_slim", ANIMATION_SPEED, "linear");
-			console.log($(this).text());
 		});
 	}, function() {
 
